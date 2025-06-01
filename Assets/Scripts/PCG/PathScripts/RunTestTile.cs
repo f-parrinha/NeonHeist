16d7596a92a6b4ae7d9604ae0b9d44566c0f49df
Liliane <lmo.correia@campus.fct.unl.pt>
2025-06-01 07:02:11 +0100

    commit mas tem erros
    erro com 1º tile depois do start tile

--- Assets/Scripts/PCG/PathScripts/RunTestTile.cs
+++ Assets/Scripts/PCG/PathScripts/RunTestTile.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.AI.Navigation;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using static UnityEngine.EventSystems.EventTrigger;
 using Random = UnityEngine.Random;
 
@@ -19,6 +21,7 @@ public class RunTestTile : MonoBehaviour
 
     [SerializeField] private Transform _startPoint;
     [SerializeField] private List<TileTypePrefabList> _tileTypePrefabs;
+    [SerializeField] private NavMeshSurface playerNavMesh;
 
     private Dictionary<TileType, List<GameObject>> tilePrefabsByType;
     
@@ -27,6 +30,8 @@ public class RunTestTile : MonoBehaviour
         tilePrefabsByType = _tileTypePrefabs.ToDictionary(t => t.type, t => t.prefabs);
 
         Generate();
+
+        //playerNavMesh.BuildNavMesh();
 
     }
 
@@ -45,18 +50,21 @@ public class RunTestTile : MonoBehaviour
         Vector3 tileOffset = startTile.transform.position - entryPoint.position;
         Vector3 pos = _startPoint.position + (_startPoint.rotation * tileOffset);
 
-        GameObject tile = Instantiate(startTileObj, pos, _startPoint.rotation); 
+        GameObject tile = Instantiate(startTileObj, pos, _startPoint.rotation);
         
-        GenerateTiles(tile);
+        Tile currentTile = tile.GetComponent<Tile>();
+        
+        GenerateTiles(currentTile);
       
     }
 
-    private void GenerateTiles(GameObject firstTile)
+    private void GenerateTiles(Tile firstTile)
     {
+        //ERRO COM 1º TILE DEPOIS DO STARTILE
         
-        Tile currentTile = firstTile.GetComponent<Tile>();
+        Tile currentTile = firstTile;
 
-        for (int rep = 0; rep < 15; rep++) // how many iteration
+        for (int rep = 0; rep < 10; rep++) // how many iteration
         {
             //Get Random Type
             TileType randomType = currentTile.randomTileType();
@@ -114,20 +122,7 @@ public class RunTestTile : MonoBehaviour
             GameObject nextTile = Instantiate(nextTileObj, targetPosition, targetRotation);
 
             InstantiateEntryFiller(entryIndexToFix, entryPointList, nextTile);
-            /*for (int i = 0; i < entryIndexToFix.Count ; i++)
-            {
-                int index = entryIndexToFix[i];
-                List<GameObject> possibleWallsPrefabs = tilePrefabsByType[TileType.Wall];
-                GameObject wallTileObj = possibleWallsPrefabs[Random.Range(0, possibleWallsPrefabs.Count)];
-
-                GameObject wall = Instantiate(wallTileObj, nextTile.transform);
-                wall.transform.localPosition = entryPointList[index].localPosition + new Vector3(0,0.45f,1.8f); //??
-                wall.transform.localRotation = entryPointList[index].localRotation;
-
-              
-                
-            }
-            */
+            
 
             nextTile.layer = LayerMask.NameToLayer("PCG");
 
@@ -136,18 +131,7 @@ public class RunTestTile : MonoBehaviour
 
         InstantiateLastWall(currentTile);
 
-        /*for (int i = 0; i < currentTile.getExitPoints().Count; i++)
-        {
-            GameObject lastTile = currentTile.gameObject;
-            List<GameObject> possibleEndPrefabs = tilePrefabsByType[TileType.End];
-            GameObject wallTileObj = possibleEndPrefabs[Random.Range(0, possibleEndPrefabs.Count)];
-
-            GameObject lastWall = Instantiate(wallTileObj, lastTile.transform);
-            lastWall.transform.localPosition = currentTile.getExitPoints()[i].transform.localPosition + (currentTile.getExitPoints()[i].transform.localRotation * new Vector3(0, 0.45f,-1.8f) ) ; //?? * rotation?
-            lastWall.transform.localRotation = currentTile.getExitPoints()[i].transform.localRotation; 
-            Debug.Log("instantiated last wall");
-
-        }*/
+       
 
     }
 
